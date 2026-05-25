b0a8ada18696f43278b4cd585a636232a0945519
Paul Roberts <dr.paul.roberts@gmail.com>
2026-05-25 12:24:12 +0100

    update

--- components/tuya_ble_node/sensor.py
+++ components/tuya_ble_node/sensor.py
@@ -2,9 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.components import tuya_ble_node
-from esphome.const import CONF_ID, CONF_NAME
-
-from .. import tuya_ble_node_ns
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ["tuya_ble_node"]
 
