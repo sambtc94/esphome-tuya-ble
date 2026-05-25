24d2cf41a965ff76a8c797e4e9df6f22e7d43314
Paul Roberts <dr.paul.roberts@gmail.com>
2026-05-25 12:15:16 +0100

    update

--- components/tuya_ble_node/sensor/__init__.py
+++ components/tuya_ble_node/sensor/__init__.py
@@ -1,29 +1,31 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.components.tuya_ble_node import (
-    CONF_TUYA_BLE_NODE_ID,
-    TUYA_BLE_NODE_SCHEMA,
-)
-from esphome.const import CONF_ID
+from esphome.components import tuya_ble_node
+from esphome.const import CONF_ID, CONF_NAME
+
+from .. import tuya_ble_node_ns
 
 DEPENDENCIES = ["tuya_ble_node"]
-CODEOWNERS = []
 
 CONF_DP_ID = "dp_id"
 CONF_SCALE = "scale"
 
-CONFIG_SCHEMA = TUYA_BLE_NODE_SCHEMA.extend(
+CONFIG_SCHEMA = cv.All(
     sensor.sensor_schema().extend(
         {
             cv.Required(CONF_DP_ID): cv.int_range(min=1, max=255),
             cv.Optional(CONF_SCALE, default=1.0): cv.float_,
         }
     )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(tuya_ble_node.TUYA_BLE_NODE_SCHEMA)
 )
 
 
 async def to_code(config):
-    node = await cg.get_variable(config[CONF_TUYA_BLE_NODE_ID])
     sens = await sensor.new_sensor(config)
+    await cg.register_component(sens, config)
+
+    node = await cg.get_variable(config[tuya_ble_node.CONF_TUYA_BLE_NODE_ID])
     cg.add(node.add_dp_sensor(config[CONF_DP_ID], config[CONF_SCALE], sens))
