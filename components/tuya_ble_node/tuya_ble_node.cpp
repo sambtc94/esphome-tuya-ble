f403f339d3d5e9f8e57375c8507689195e03ad95
sambtc94 <sambtc1994@gmail.com>
2026-08-19 17:34:00 +1000

    Add set_dp_enum method for setting DP enum values

--- components/tuya_ble_node/tuya_ble_node.cpp
+++ components/tuya_ble_node/tuya_ble_node.cpp
@@ -150,5 +150,23 @@ void TuyaBLENode::set_dp_bool(uint8_t dp_id, bool value) {
   this->enqueue_command(&command);
 }
 
+void TuyaBLENode::set_dp_enum(uint8_t dp_id, uint8_t value) {
+
+  if(!this->has_client) {
+    ESP_LOGW(TAG, "No client registered at node");
+    return;
+  }
+
+  TYBLECommand command = {
+    TuyaBLECode::FUN_SENDER_DPS,
+    { dp_id, 0x04, 0x01, value },
+    this->session_key,
+    0,
+    3,
+  };
+
+  this->enqueue_command(&command);
+}
+
 }  // namespace tuya_ble_node
 }  // namespace esphome
